ba520dcfae526ecb1f95eb6291ca998b8c5c91c1
shwethaj1104 <shwethaj1104@gmail.com>
2022-08-05 18:37:09 +0530

    Data updated

--- src/data.js
+++ src/data.js
@@ -5,15 +5,19 @@ const menu = [
     category: 'breakfast',
     price: 15.99,
     img: './images/item-1.jpeg',
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    desc: 'A pancake made with buttermilk. type of: battercake, flannel cake, flannel-cake, flapcake, flapjack, griddlecake, hot cake, hotcake, pancake. a flat cake of thin batter fried on both sides on a griddle.',
   },
   {
     id: 2,
-    title: 'diner double',
+    title: 'Burger with french fries',
     category: 'lunch',
     price: 13.99,
     img: './images/item-2.jpeg',
-    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    desc: `Lentil and Mushroom Burger.
+    Stuffed Bean Burger.
+    Lamb Burger with Radish Slaw.
+    Potato Corn Burger.
+    Supreme Veggie Burger.`,
   },
   {
     id: 3,
@@ -21,7 +25,7 @@ const menu = [
     category: 'shakes',
     price: 6.99,
     img: './images/item-3.jpeg',
-    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    desc: `Milo blended with banana & mint chocolate chip ice cream, drizzled with melted chocolate & topped with more ice cream`,
   },
   {
     id: 4,
@@ -29,7 +33,7 @@ const menu = [
     category: 'breakfast',
     price: 20.99,
     img: './images/item-4.jpeg',
-    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    desc: `A well-done bread toast will be golden brown or light brown on both sides and have a crunchy and dry texture. It is best to have toasts which are fresh out of the toaster as they tend to become stiff after a while. `,
   },
   {
     id: 5,
@@ -37,7 +41,7 @@ const menu = [
     category: 'lunch',
     price: 22.99,
     img: './images/item-5.jpeg',
-    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    desc: `The egg burger is a simple twist on the classic burger, substituting the other patty for an egg. Like any burger, dress it up or down with your own personal combination of toppings and condiments.`,
   },
   {
     id: 6,
@@ -45,7 +49,7 @@ const menu = [
     category: 'shakes',
     price: 18.99,
     img: './images/item-6.jpeg',
-    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    desc: `Thick Oreo Milkshake is a delicious beverage made using Oreo cookies, whole milk, and vanilla ice cream`,
   },
   {
     id: 7,
@@ -53,7 +57,7 @@ const menu = [
     category: 'breakfast',
     price: 8.99,
     img: './images/item-7.jpeg',
-    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    desc: `There's nothing better than bacon recipes when you want something savory! From mac and cheese to soup to pizza to burgers,`,
   },
   {
     id: 8,
@@ -61,7 +65,7 @@ const menu = [
     category: 'lunch',
     price: 12.99,
     img: './images/item-8.jpeg',
-    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    desc: `Stack patties, bacon, onion on burger bun bases. Top with lettuce, tomato and pickles. Add mustard and mayonnaise.`,
   },
   {
     id: 9,
@@ -69,7 +73,7 @@ const menu = [
     category: 'shakes',
     price: 16.99,
     img: './images/item-9.jpeg',
-    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    desc: ` Lemonade with iced-tea cubes,Schuyler spritzer,Watermelon mojito`,
   },
 ];
 export default menu;
